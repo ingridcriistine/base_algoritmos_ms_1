fdba867bd63a49153025ba70931d71e106940f03
ingridcriistine <ingridrochacristine@gmail.com>
2024-09-13 17:05:43 -0300

    exercicio 18

---
+++ ex18.py
@@ -0,0 +1,6 @@
+#https://github.com/Andredev-dias/Base-de-Algoritmos-em-C/blob/master/if01.c
+
+salario = int(input("Informe o salário: "))
+
+if salario > 5000:
+    print("Salário maior R$ 5000,00")
