afeebb1813711605875a67261bd5385bf6a15330
ingridcriistine <ingridrochacristine@gmail.com>
2024-09-13 15:12:42 -0300

    exercicio 7

---
+++ ex7.py
@@ -0,0 +1,11 @@
+#https://github.com/Andredev-dias/Base-de-Algoritmos-em-C/blob/master/aulapratica26.c
+
+hr1 = int(input("Informe as horas trabalhadas no primeiro dia: "))
+hr2 = int(input("Informe as horas trabalhadas no segundo dia: "))
+hr3 = int(input("Informe as horas trabalhadas no terceiro dia: "))
+hr4 = int(input("Informe as horas trabalhadas no quarto dia: "))
+hr5 = int(input("Informe as horas trabalhadas no quinto dia: "))
+
+media = (hr1+hr2+hr3+hr4+hr5)/5
+
+print("\nA média de horas trabalhadas é de: " + str(media))
