3dc2f781ea85236dddffefbedbbff8753580c371
ingridcriistine <ingridrochacristine@gmail.com>
2024-09-13 17:03:35 -0300

    exercicio 17

---
+++ ex17.py
@@ -0,0 +1,8 @@
+#https://github.com/Andredev-dias/Base-de-Algoritmos-em-C/blob/master/if03.c
+
+num = int(input("Informe um número: "))
+
+if num % 2 == 0:
+    print("Este número é par.")
+else:
+    print("Este número é ímpar.")
